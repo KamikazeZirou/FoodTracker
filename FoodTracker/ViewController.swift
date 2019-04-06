6577d77c067c04e3d3078b59f5e6cee8ee1ff817
KamikazeZirou <kamikaze.zirou@gmail.com>
2019-04-06 20:52:46 +0900

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by kamikaze on 2019/04/06.
+//  Copyright © 2019 kamikaze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
